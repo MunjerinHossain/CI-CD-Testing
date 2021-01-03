1c3fa4384dc4ba363eb5f457873acb0736dc2e9f
Munjerin Hossain <munjerin15muna@gmail.com>
2021-01-03 19:42:12 +0600

    PUT test is working.

--- _test_/app.test.js
+++ _test_/app.test.js
@@ -42,6 +42,20 @@ describe("When the article CRUD server is running", () => {
     expect(res.status).toBe(400);
   });
 
+  it("should return an article matching the ID specified in a PUT request to '/article/:id'", async () => {
+    const allArticles = await request(app).get("/articles/all");
+    const id = allArticles.body[0]._id;
+    console.log(id);
+    const res = await request(app).put("/articles/" + id);
+    expect(res.status).toBe(200);
+    expect(res.body.title).toEqual("test");
+  });
+
+  it("should return 400 if an article cannot match the ID specified in a PUT request to '/article/:id'", async () => {
+    const res = await request(app).put("/articles/hghj");
+    expect(res.status).toBe(400);
+  });
+
   afterAll(async () => {
     await server.close();
     await mongoose.connection.db.dropDatabase();
--- app.js
+++ app.js
@@ -47,7 +47,17 @@ app.get('/articles/:id', function(request, response){
         }
         return response.status(200).json(data)
     })
-})   
+})
+
+app.put('/articles/:id', function(request, response){
+  let id = request.params.id
+  Article.findById(id, function(err, data){
+      if(err){
+          return response.status(400).json({error: 'Article not found'})
+      }
+      return response.status(200).json(data)
+  })
+})
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", () => {
   console.log("Server running");
